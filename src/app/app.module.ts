e67faf0c70ad9781bec36ae1dffdc97c122cdc93
faheem <faheem@faheems-MacBook-Air.local>
2018-10-31 17:33:43 +0530

    adding the properties for elector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
